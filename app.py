6cb272f88f6f20050622ff4e247bdf9b89bf8451
RealdoBeja98 <realdobeja@gmail.com>
2020-10-21 01:24:32 +0200

    Projects

--- app.py
+++ app.py
@@ -2,10 +2,12 @@ from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
 @app.route('/')
-def base():
-    return render_template('home.html') 
+def home():
+    return render_template('home.html', title = 'Home | The Pallets Project') 
 
-
+@app.route('/projects')
+def projects():
+    return render_template('projects.html', title = 'Projects | The Pallets Project')
 
 if __name__ == "__main__":
     app.run(debug=True)
