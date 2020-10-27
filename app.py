c4de0b0eccaca765e35d47a696ecbecb7099642d
RealdoBeja98 <realdobeja@gmail.com>
2020-10-27 15:19:24 +0100

    Governance

--- app.py
+++ app.py
@@ -9,6 +9,15 @@ def home():
 def projects():
     return render_template('projects.html', title = 'Projects | The Pallets Project')
 
+@app.route('/governance')
+def governance():
+    return render_template('governance.html', title = 'Governance | The Pallets Project')
+
+
+
+
+
+
 if __name__ == "__main__":
     app.run(debug=True)
 
