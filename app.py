edc17fe5abdbae5aa485752f7852d532ee1f266f
Endi Sukaj <endisukaj@gmail.com>
2020-11-02 23:10:07 +0100

    black formatting

--- app.py
+++ app.py
@@ -1,15 +1,23 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template
+import sqlite3
+
 app = Flask(__name__)
 
-@app.route('/')
+conn = sqlite3.connect("./blog.db")
+c = conn.cursor()
+
+
+@app.route("/")
 def home():
-    return render_template('home.html', title = 'Home | The Pallets Project') 
+    return render_template("home.html", title="Home | The Pallets Project")
 
-@app.route('/projects')
+
+@app.route("/projects")
 def projects():
-    return render_template('projects.html', title = 'Projects | The Pallets Project')
+    return render_template("projects.html", title="Projects | The Pallets Project")
 
-@app.route('/governance')
+
+@app.route("/governance")
 def governance():
     return render_template('governance.html', title = 'Governance | The Pallets Project')
 
